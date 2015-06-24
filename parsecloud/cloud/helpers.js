fa8a752e42618be32a514d19406e696cb8ea4620
Flauen <Flauen@Flauen-PC>
2015-06-23 23:07:14 -0300

    generado cloud code y helpers principales

---
+++ parsecloud/cloud/helpers.js
@@ -0,0 +1,160 @@
+﻿var Image = require('parse-image');
+
+exports.log = function(){
+  var ret = '';
+  for (var i = 0; i < arguments.length; i++) {
+    if(typeof(arguments[i]) == 'object'){
+      ret+=' ' + JSON.stringify(arguments[i]);
+    }else{
+      ret+=' ' + arguments[i];
+    }
+  }
+  if(ret){
+    console.log('----------------------- LOG START ------------------------');
+    console.log(ret);
+    console.log('------------------------ LOG END -------------------------');
+  }
+}
+
+exports.sendEmail = function(toAddress,subject, htmlstring)
+{
+  var Mandrill = require('mandrill');
+  Mandrill.initialize('vHa8A1DV1EfoY7INdZATbQ');
+  Mandrill.sendEmail({   
+    "message": {
+      "html" : htmlstring,
+      "text" : "Reserved for spam",
+      "subject": subject,
+      "from_email": "alexdedek@hotmail.com",
+      "from_name": "Alex",
+      "to": toAddress,
+    },
+    async: true
+  },{
+    success: function() {
+      //response.success("Email sent!");
+    },
+    error: function(error) {
+      console.error('Error Sending Emails! ' + JSON.stringify(error));
+      //response.error("Uh oh, something went wrong");
+    }
+  });
+};
+
+//thumbnail treatment
+exports.resizePicture = function(picture, filename, width, height, force)
+{
+  var promise = new Parse.Promise();
+  Parse.Cloud.httpRequest({
+    url: picture
+  }, function (error) {
+    promise.reject(error);
+  }).then(function (response) {
+    var image = new Image();
+    return image.setData(response.buffer);
+  }, function (error) {
+    promise.reject(error);
+  }).then(function (image) {
+    // transformar en cuadrado tomando el menor valor entre altura y ancho
+    if(force || image.width() > width || image.height() > height){
+      var ratio = image.width()/image.height();
+      if(ratio < 1) {
+        width = height * ratio;
+      }else {
+        height = width / ratio;
+      }
+    }else{
+      // JPEG es mas liviano
+      return image.setFormat('JPEG');
+    }
+    // resize
+    return image.scale({
+      width: width,
+      height: height
+    }).then(function (image) {
+      // JPEG es mas liviano
+      return image.setFormat('JPEG');
+    }, function (error) {
+      promise.reject(error);
+    });
+  }, function (error) {
+    promise.reject(error);
+  }).then(function (image) {
+    // Poner la data en un buffer
+    return image.data();
+  }, function (error) {
+    promise.reject(error);
+  }).then(function (buffer) {
+    // guardar en un file
+    filename = filename.substr(0, filename.lastIndexOf('.'));
+    var base64 = buffer.toString('base64');
+    var resized = new Parse.File(filename + '.jpg', { base64: base64 });
+    //return 
+    return resized.save();
+  }, function (error) {
+    promise.reject(error);
+  }).then(function (image) {
+    promise.resolve(image);
+  }, function (error) {
+    promise.reject(error);
+  });
+  return promise;
+};
+
+//thumbnail treatment
+exports.createThumbnail = function(picture, filename, cropsize)
+{
+  var promise = new Parse.Promise();
+  Parse.Cloud.httpRequest({
+    url: picture
+  }, function (error) {
+    promise.reject(error);
+  }).then(function (response) {
+    var image = new Image();
+    return image.setData(response.buffer);
+  }, function (error) {
+    promise.reject(error);
+  }).then(function (image) {
+    // transformar en cuadrado tomando el menor valor entre altura y ancho
+    var size = Math.min(image.width(), image.height());
+    return image.crop({
+      left: (image.width() - size) / 2,
+      top: (image.height() - size) / 2,
+      width: size,
+      height: size
+    });
+  }, function (error) {
+    promise.reject(error);
+  }).then(function (image) {
+    // resize
+    return image.scale({
+      width: cropsize,
+      height: cropsize
+    });
+  }, function (error) {
+    promise.reject(error);
+  }).then(function (image) {
+    // JPEG es mas liviano
+    return image.setFormat('JPEG');
+  }, function (error) {
+    promise.reject(error);
+  }).then(function (image) {
+    // Poner la data en un buffer
+    return image.data();
+  }, function (error) {
+    promise.reject(error);
+  }).then(function (buffer) {
+    // guardar en un file
+    filename = filename.substr(0, filename.lastIndexOf('.'));
+    var base64 = buffer.toString('base64');
+    var cropped = new Parse.File(filename + '.jpg', { base64: base64 });
+    return cropped.save();
+  }, function (error) {
+    promise.reject(error);
+  }).then(function (image) {
+    promise.resolve(image);
+  }, function (error) {
+    promise.reject(error);
+  });
+  return promise;
+};
---
+++ parsecloud/cloud/main.js
@@ -0,0 +1,125 @@
+
+// Use Parse.Cloud.define to define as many cloud functions as you want.
+//Included some helpers =)
+var helpers = require('cloud/helpers.js');
+ 
+// OBJECT USER
+ 
+// beforeSave: USER -- Data Validation
+Parse.Cloud.beforeSave(Parse.User, function (request, response) {
+ 
+  //regex definition
+  var checkPhoneNumber = /^(\+)?\d{10,20}$/;///^\d{3}\d{3}\d{4}|\d{10}/;
+  var checkLength = /^([a-zA-Z0-9_-]){4,16}$/;
+  var checkName = /^[a-zA-Z0-9_-]+$/;
+  var emailFormat = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+ 
+  //
+  var user = request.object;
+  var isNew = !user.id;
+   
+  //add trimming
+  var phoneNumber = user.get('phoneNumber');
+  if(phoneNumber)phoneNumber=phoneNumber.trim();
+ 
+  var username = user.get('username');
+  if(username)username=username.trim();
+ 
+  var name = user.get('name');
+  if(name)name=name.trim();
+ 
+  var lastName = user.get('lastName');
+  if(lastName)lastName=lastName.trim();
+ 
+  var email = user.get('email');
+  if(email)email=email.trim();
+ 
+  var companyName = user.get('companyName');
+  if(companyName)companyName=companyName.trim();
+ 
+  var address = user.get('address');
+  if(address)address=address.trim();
+
+  var expirationTime = user.get('expirationTime');
+  if(expirationTime)expirationTime = Date.parse(expirationTime);
+ 
+  if ((isNew && !phoneNumber) || (phoneNumber && !checkPhoneNumber.test(phoneNumber))) {
+    response.error('Formato de número de teléfono incorrecto, debe tener al menos 10 dígitos');
+  }
+  else if ((isNew && !username) || (username && (username.length < 4 || username.length > 16))) {
+    response.error('El nombre de usuario debe tener entre 4 y 16 caracteres');
+  }
+  else if ((isNew && !username) || (username && !checkName.test(username))) {
+    response.error('El nombre de usuario no debe contener caracteres especiales ni espacios en blanco');
+  } 
+  else if ((isNew && !name) || (name && (name.length < 2 || name.length > 32))) {
+    response.error('Debe especificar un nombre');
+  }
+  else if ((isNew && !lastName) || (lastName && (lastName.length < 2 || lastName.length > 32))) {
+    response.error('Debe especificar un apellido');
+  }
+  else if ((isNew && !email) || (email && !emailFormat.test(email))) {
+    response.error('Formato de email incorrecto');
+  } 
+  else if (companyName || address) {
+    if ((isNew && !companyName) || (companyName && !companyName.length)) {
+      response.error('Como vendedor, debe especificar una dirección');
+    }
+    else if ((isNew && !address) || (address && !address.length)) {
+      response.error('Como vendedor, debe especificar una dirección');
+    }
+    else if ((isNew && !expirationTime) || (expirationTime && isNaN(expirationTime))) {
+      response.error('Como vendedor, debe especificar una hora de cierre');
+    }
+    else{
+      response.success();
+    }
+  }else{
+    response.success();
+  }
+ 
+  //crear thumbnails...
+  /*helpers.createThumbnail(user.get('avatar'),username + '_thumb',96).
+  then(function (cropped) {
+    user.set('thumbnail', cropped);
+  }).then(function (result) {
+    response.success();
+  }, function (error) {
+    response.error(error)
+  });*/
+ 
+});
+ 
+// afterSave: USER -- role setting
+Parse.Cloud.afterSave(Parse.User, function(request) {
+  var user = request.object;
+  if (!user.existed()) {
+    Parse.Cloud.useMasterKey();
+    //only assings role if it isn't a regular user(client/buyer)
+    query = new Parse.Query(Parse.Role);
+    if (user.get('companyName')) {
+      query.equalTo("name", "teacher");
+    } else {
+      query.equalTo("name", "student");
+    }
+    query.first ( {
+      success: function(object) {
+        object.relation("users").add(request.user);
+        object.save(null,{
+          success: function (us) {
+            //response.success();
+          },
+          error: function (us, error) {
+            //response.error('Ocurrió un error en la asignación de roles: ' + error.message)
+          }
+        });
+      },
+      error: function(error) {
+        throw "Got an error " + error.code + " : " + error.message;
+        //response.error('Ocurrió un error en la asignación de roles: ' + error.message);
+      }
+    });
+  } else {
+    //response.success();
+  }
+});
