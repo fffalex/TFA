099d07c70a0b307cdb29958970b57b941a020325
Alexander <Flauen@Flauen-PC>
2016-01-04 09:23:44 -0300

    Aplicado el estilo de los modals

--- app/scripts/controllers/student/contents/classes.js
+++ app/scripts/controllers/student/contents/classes.js
@@ -4,56 +4,82 @@ angular.module('tfaApp')
   .controller('StudentClassesCtrl', function ($scope, unitsrv, coursesrv) {
 
     //courses call
-    $scope.fullUnits = [];
-    var coursesArr = [];
+    //initial data set
+    $scope.unit = {};
+    $scope.topics = [];
+    $scope.currentTopic = {};
+    $scope.error = "";
+    $scope.success = "";
 
-    $scope.xalert = function () {
-        console.log("Se apreto todito");
-    }
-    
-    $scope.wasSeen = function (unitIndex, TopicIndex) {
+    //Control function flows
+    $scope.editable = false;
+    $scope.creating = false;
+
+    //Take objectID to query from the routeParams
+    var objectId = 0;
+    var blockId = 0;
+    var topicId = 0;
+    if ($routeParams) {
+      objectId = $routeParams.objectId;
+      topicId = $routeParams.secondId;
+      blockId = $routeParams.blockId;
 
     }
-    var course = Parse.User.current().get('assignedTo');
-    var studentId = Parse.User.current().get('id');
-    
-    //to get curse units
-    unitsrv.getAllUnits(course, {
-                success: function(units){
-                    $scope.fullUnits = units;
-                    $scope.currentUnit = $scope.fullUnits[0];
-                    $scope.currentTopic = $scope.currentUnit.topicsJSON[0];
-                     
-                    //for (var i = 0; i < $scope.fullUnits.length; i++) {
-                    //    for (var j = 0; j < $scope.fullUnits.topicsJSON.length; j++) {
-                    //        if($scope.fullUnits.topicsJSON[j].seenBy == studentId){
-                    //            $scope.fullUnits.topicsJSON[j].
-                    //        }
-                    //    };
-                    //};
-                    $scope.$apply();
-                },
-                error: function(error){
-                    console.log(error);
-                }
-              });
-  
-    $scope.showUnit = function (index) {
-          $scope.currentUnit = $scope.fullUnits[index];
-          $scope.currentTopic = $scope.fullUnits[index].topicsJSON[0];
-          $scope.apply;
-      };
-      
-    $scope.showTopic = function (index,unitId) {
-          var result =  $.grep($scope.fullUnits, function(e){ return e.objectId == unitId; });
-          $scope.currentUnit = result[0];
-          $scope.currentTopic = $scope.currentUnit.topicsJSON[index];
-          $scope.apply;
-      };
-  
-  
-  
-  
-  });
-  
-           
+    $scope.unitId = objectId;
+    $scope.blockId = blockId;
+    var flagUnitId = false;
+    //Initial query to set te Unit and Topic array
+    var query = new Parse.Query('Unit');
+    query.equalTo('objectId', objectId);
+    query.include('topics');
+    query.first({
+      success: function (unit) {
+        $scope.unit = unit;
+        $scope.topics = [];
+        if (unit.get('topics') !== undefined) {
+          for (var i = 0; i < unit.get('topics').length; i++) {
+            if (unit.get('topics')[i].get('status') == 1) {
+              $scope.topics.push(unit.get('topics')[i]);
+            }
+
+            if (unit.get('topics')[i].id == topicId) {
+              $scope.currentTopic = unit.get('topics')[i];
+              $scope.currentTitle  = $scope.currentTopic.get('title');
+              $scope.currentNumber  = $scope.currentTopic.get('number');
+              $scope.currentContent = $scope.currentTopic.get('content');
+              $scope.currentTopicCopy = angular.copy($scope.currentUnit);
+              flagUnitId = true;
+            }
+          }
+          $scope.topics = sortByKey($scope.topics, "number");
+          if (!flagUnitId) {
+            $scope.currentTopic = $scope.topics[0];
+            $scope.currentTitle  = $scope.currentTopic.get('title');
+            $scope.currentNumber  = $scope.currentTopic.get('number');
+            $scope.currentContent = $scope.currentTopic.get('content');
+            $scope.currentTopicCopy = angular.copy($scope.currentTopic);
+          }
+          $scope.$apply();
+        } else {
+          $scope.error = "No hay ningun tema"
+
+        }
+      },
+      error: function (error) {
+        $scope.error = "TODO MAL CHABON";
+      }
+    });
+
+    //To set view of Details about Topic
+    $scope.showTopic = function (index) {
+      $scope.editable = false;
+      $scope.creating = false;
+      $scope.currentTopic = $scope.topics[index];
+      $scope.currentTitle  = $scope.currentTopic.get('title');
+      $scope.currentNumber  = $scope.currentTopic.get('number');
+      $scope.currentContent = $scope.currentTopic.get('content');
+      $scope.currentTopicCopy = angular.copy($scope.currentTopic);
+      $scope.apply;
+    };
+
+});
